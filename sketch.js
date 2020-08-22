8973e2c869a8601a3e3faf4d02e1c4c43df50bf7
puru2007 <69470117+puru2007@users.noreply.github.com>
2020-08-22 12:44:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50, 200, 50, 10);
+  
+  
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=(80,80,80);
+
+ 
+ 
+  speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+}
+var bullet,wall,thickness;
+var speed,weight;
+
+
+function draw() {
+  background(255,255,255); 
+  bullet.velocityX=speed;
+  hasCollided();
+  if(hasCollided(bullet,wall))
+  {
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(hickness*thisckness*thickness);
+    if(damage>10)
+    {
+      wall.shapeColor=color(255,0,0);
+    }
+    if(damage<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+
+  
+  
+
+
+  drawSprites();
+}
+function hasCollided(bullet, wall)
+{
+  bulletightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+}
